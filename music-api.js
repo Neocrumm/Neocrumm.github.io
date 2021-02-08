6f0b1b05b923e496b440ca61d800423510633183
parker <karasolomon2010@gmail.com>
2021-02-07 18:28:48 -0800

    Update music-api.js

--- music-api.js
+++ music-api.js
@@ -1,3 +1,3 @@
 // add your faves on the musicfaves array.
 var musicfaves = ["Its Raining Tacos By Parry Gripp", "Fall Back Down By Rancid", "Ruby SOHO By Rancid", "Here Comes The Hammer By MC Hammer"]
-document.getElementById("music").innerHTML = muscfaves
+document.getElementById("music").innerHTML = musicfaves
